b5ac6fdad96c67b743d571b11e7f74d7dbc190c4
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2024-12-31 23:07:39 +0100

    Finish off tests

--- src/continuous_timeseries/timeseries_discrete.py
+++ src/continuous_timeseries/timeseries_discrete.py
@@ -188,20 +188,27 @@ class TimeseriesDiscrete:
                 )
 
             except ImportError:
-                units_registered_with_matplotlib = False
+                msg = (
+                    "Could not import `matplotlib.units` "
+                    "to set up unit-aware plotting. "
+                    "We will simply try plotting magnitudes instead."
+                )
+                warnings.warn(msg, stacklevel=3)
+
+                return pint_q.m
 
             if units_registered_with_matplotlib:
                 plot_vals = pint_q
             else:
                 if warn_if_plotting_magnitudes:
                     msg = (
-                        f"The units of {desc} are not registered with matplotlib. "
+                        f"The units of `{desc}` are not registered with matplotlib. "
                         "The magnitude will be plotted "
                         "without any consideration of units. "
-                        "For docs on how to set up unit-aware plotting, see: "
-                        "https://pint.readthedocs.io/en/stable/user/plotting.html"
+                        "For docs on how to set up unit-aware plotting, see "
+                        "[the stable docs](https://pint.readthedocs.io/en/stable/user/plotting.html) "  # noqa: E501
                         "(at the time of writing, the latest version's docs were "
-                        "https://pint.readthedocs.io/en/0.24.4/user/plotting.html)"
+                        "[v0.24.4](https://pint.readthedocs.io/en/0.24.4/user/plotting.html))."
                     )
                     warnings.warn(msg, stacklevel=3)
 
--- tests/unit/test_timeseries_discrete.py
+++ tests/unit/test_timeseries_discrete.py
@@ -156,23 +156,123 @@ def test_html(ts, file_regression):
 
 
 @pytest.mark.parametrize(
-    "plot_kwargs, legend",
+    "x_units, y_units, plot_kwargs",
     (
-        pytest.param({}, False, id="defaults"),
-        pytest.param({}, True, id="legend"),
+        pytest.param(None, None, {}, id="no-units-set"),
+        pytest.param("month", None, {}, id="x-units-set"),
+        pytest.param(None, "t / yr", {}, id="y-units-set"),
+        pytest.param("s", "Gt / yr", {}, id="x-and-y-units-set"),
+        pytest.param(
+            "yr", "Gt / yr", dict(alpha=0.7, s=130), id="x-and-y-units-set-kwargs"
+        ),
+    ),
+)
+def test_plot(x_units, y_units, plot_kwargs, image_regression, tmp_path):
+    import matplotlib
+
+    # ensure matplotlib does not use a GUI backend (such as Tk)
+    matplotlib.use("Agg")
+
+    import matplotlib.pyplot as plt
+    import matplotlib.units
+
+    # Setup matplotlib to use units
+    UR.setup_matplotlib(enable=True)
+
+    fig, ax = plt.subplots()
+
+    mt_month = TimeseriesDiscrete(
+        name="Mt per month",
+        time_axis=TimeAxis(Q([2020.0, 2021.0, 2022.0], "yr")),
+        values_at_bounds=ValuesAtBounds(Q([10.0, 10.5, 11.0], "Mt / month") * 100.0),
+    )
+
+    gt_yr = TimeseriesDiscrete(
+        name="Gt per year",
+        time_axis=TimeAxis(Q([2020.0, 2021.0, 2022.0], "yr")),
+        values_at_bounds=ValuesAtBounds(Q([10.0, 10.5, 11.0], "Gt / yr")),
+    )
+
+    mt = TimeseriesDiscrete(
+        name="Mt",
+        time_axis=TimeAxis(Q([2020.0, 2021.0, 2022.0], "yr")),
+        values_at_bounds=ValuesAtBounds(Q([10.0, 10.5, 11.0], "Mt")),
+    )
+
+    if x_units is not None:
+        ax.set_xlabel(x_units)
+        ax.xaxis.set_units(x_units)
+
+    if y_units is not None:
+        ax.set_ylabel(y_units)
+        ax.yaxis.set_units(y_units)
+
+    # Even though timeseries are in different units,
+    # use of pint with matplotib will ensure sensible units on plot.
+    mt_month.plot(ax=ax, **plot_kwargs)
+    gt_yr.plot(ax=ax, **plot_kwargs)
+
+    # Trying to plot something with incompatible units will raise.
+    with pytest.raises(matplotlib.units.ConversionError):
+        mt.plot(ax=ax, **plot_kwargs)
+
+    ax.legend()
+
+    fig.tight_layout()
+
+    out_file = tmp_path / "fig.png"
+    fig.savefig(out_file)
+
+    image_regression.check(out_file.read_bytes(), diff_threshold=0.01)
+
+    # Ensure we tear down
+    UR.setup_matplotlib(enable=False)
+
+
+@pytest.mark.parametrize(
+    "plot_kwargs, legend, expectation",
+    (
+        pytest.param(
+            {},
+            False,
+            pytest.warns(
+                UserWarning,
+                match=re.escape(
+                    "The units of `self.values_at_bounds.values` "
+                    "are not registered with matplotlib. "
+                    "The magnitude will be plotted without any consideration of units. "
+                    "For docs on how to set up unit-aware plotting, see "
+                    "[the stable docs](https://pint.readthedocs.io/en/stable/user/plotting.html) "  # noqa: E501
+                    "(at the time of writing, the latest version's docs were "
+                    "[v0.24.4](https://pint.readthedocs.io/en/0.24.4/user/plotting.html))."
+                ),
+            ),
+            id="defaults",
+        ),
+        pytest.param({}, True, pytest.warns(UserWarning), id="legend"),
+        pytest.param(
+            dict(warn_if_plotting_magnitudes=False),
+            True,
+            does_not_raise(),
+            id="no-warning",
+        ),
         pytest.param(
             dict(label="custom"),
             True,
+            pytest.warns(UserWarning),
             id="label-overwriting",
         ),
         pytest.param(
             dict(marker="x", color="tab:green", label="demo"),
             True,
+            pytest.warns(UserWarning),
             id="kwargs-passing",
         ),
     ),
 )
-def test_plot(plot_kwargs, legend, image_regression, tmp_path):
+def test_plot_matplotlib_no_units(
+    plot_kwargs, legend, expectation, image_regression, tmp_path
+):
     import matplotlib
 
     # ensure matplotlib does not use a GUI backend (such as Tk)
@@ -210,70 +310,11 @@ def test_plot(plot_kwargs, legend, image_regression, tmp_path):
         ),
     )
 
-    ts.plot(ax=ax, **plot_kwargs)
+    with expectation:
+        ts.plot(ax=ax, **plot_kwargs)
+
     if legend:
         ax.legend()
-
-    out_file = tmp_path / "fig.png"
-    fig.savefig(out_file)
-
-    image_regression.check(out_file.read_bytes(), diff_threshold=0.01)
-
-
-@pytest.mark.parametrize(
-    "x_units, y_units",
-    (
-        pytest.param(None, None, id="no-units-set"),
-        pytest.param("month", None, id="x-units-set"),
-        pytest.param(None, "t / yr", id="y-units-set"),
-        pytest.param("s", "Gt / yr", id="x-and-y-units-set"),
-    ),
-)
-def test_plot_matplotlib_units(x_units, y_units, image_regression, tmp_path):
-    import matplotlib
-
-    # ensure matplotlib does not use a GUI backend (such as Tk)
-    matplotlib.use("Agg")
-
-    import matplotlib.pyplot as plt
-    import matplotlib.units
-
-    UR.setup_matplotlib(enable=True)
-
-    fig, ax = plt.subplots()
-
-    mt_month = TimeseriesDiscrete(
-        name="Mt per month",
-        time_axis=TimeAxis(Q([2020.0, 2021.0, 2022.0], "yr")),
-        values_at_bounds=ValuesAtBounds(Q([10.0, 10.5, 11.0], "Mt / month") * 100.0),
-    )
-
-    gt_yr = TimeseriesDiscrete(
-        name="Gt per year",
-        time_axis=TimeAxis(Q([2020.0, 2021.0, 2022.0], "yr")),
-        values_at_bounds=ValuesAtBounds(Q([10.0, 10.5, 11.0], "Gt / yr")),
-    )
-
-    mt = TimeseriesDiscrete(
-        name="Mt",
-        time_axis=TimeAxis(Q([2020.0, 2021.0, 2022.0], "yr")),
-        values_at_bounds=ValuesAtBounds(Q([10.0, 10.5, 11.0], "Mt")),
-    )
-
-    # Even though timeseries are in different units,
-    # use of pint with matplotib will ensure sensible units on plot.
-    mt_month.plot(ax=ax)
-    gt_yr.plot(ax=ax)
-    with pytest.raises(matplotlib.units.ConversionError):
-        mt.plot(ax=ax)
-
-    if x_units is not None:
-        ax.xaxis.set_units(UR.Unit(x_units))
-
-    if y_units is not None:
-        ax.yaxis.set_units(UR.Unit(y_units))
-
-    ax.legend()
 
     out_file = tmp_path / "fig.png"
     fig.savefig(out_file)
@@ -303,4 +344,37 @@ def test_plot_ax_creation(sys_modules_patch, expectation):
     )
     with patch.dict(sys.modules, sys_modules_patch):
         with expectation:
-            ts.plot()
+            ts.plot(warn_if_plotting_magnitudes=False)
+
+
+@pytest.mark.parametrize(
+    "sys_modules_patch, expectation",
+    (
+        pytest.param({}, does_not_raise(), id="matplotlib_available"),
+        pytest.param(
+            {"matplotlib": None},
+            pytest.warns(
+                UserWarning,
+                match=re.escape(
+                    "Could not import `matplotlib.units` "
+                    "to set up unit-aware plotting. "
+                    "We will simply try plotting magnitudes instead."
+                ),
+            ),
+            id="matplotlib_not_available",
+        ),
+    ),
+)
+def test_plot_no_matplotlib_units(sys_modules_patch, expectation):
+    import matplotlib.pyplot as plt
+
+    _, ax = plt.subplots()
+
+    ts = TimeseriesDiscrete(
+        name="basic",
+        time_axis=TimeAxis(Q([1.0, 2.0, 3.0], "yr")),
+        values_at_bounds=ValuesAtBounds(Q([10.0, 20.0, 5.0], "kg")),
+    )
+    with patch.dict(sys.modules, sys_modules_patch):
+        with expectation:
+            ts.plot(ax=ax)
