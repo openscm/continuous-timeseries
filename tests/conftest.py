1e3ed123428c5d84c3a42758a4037d0b4659c6e3
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-01-17 16:38:47 +0100

    Add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,21 @@ Re-useable fixtures etc. for tests
 
 See https://docs.pytest.org/en/7.1.x/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
 """
+
+from __future__ import annotations
+
+import pandas as pd
+import pytest
+
+import continuous_timeseries.pandas_accessors
+
+
+@pytest.fixture()
+def setup_pandas_accessor() -> None:
+    # Not parallel safe, but good enough
+    continuous_timeseries.pandas_accessors.register_pandas_accessor()
+
+    yield None
+
+    if hasattr(pd.Series, "ct"):
+        del pd.Series.ct
